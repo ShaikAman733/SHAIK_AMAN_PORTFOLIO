87443da7ead974e9d6d87f0c7dd3d0177b148af0
Shaik Aman <shaikaman2411@gmai.com>
2025-12-15 18:53:06 +0530

    Updated

--- src/Hero/Hero.jsx
+++ src/Hero/Hero.jsx
@@ -2,9 +2,20 @@ import React from 'react';
 import ChangingText from './ChangingText';
 import './Hero.css';
 import profile from '/assets/profile.jpg';
+// Remember to install react-icons: npm install react-icons
+import { FaGithub, FaLinkedin, FaCode } from 'react-icons/fa'; 
+import { SiLeetcode } from 'react-icons/si';
 
 // Updated Resume Link
-const RESUME_LINK = "https://drive.google.com/file/d/1bKa6PvGhMqPaeWKeUI1NR18Ud3Umt8Ka/view?usp=drive_link";
+const RESUME_LINK = "https://drive.google.com/file/d/1171MfsUW4k1n4x4FnV1Gq7wPJS-ZNkcA/view?usp=drive_link";
+
+// Social/Coding Profile Links - FINAL LINKS
+const SOCIAL_LINKS = [
+    { name: 'GitHub', url: 'https://github.com/shaikaman733', icon: FaGithub },
+    { name: 'LinkedIn', url: 'https://www.linkedin.com/in/aman-shaik7/', icon: FaLinkedin },
+    { name: 'LeetCode', url: 'https://leetcode.com/u/Shaik_Aman7/', icon: SiLeetcode },
+    { name: 'CodeWars', url: 'https://www.codewars.com/users/Shaikaman', icon: FaCode },
+];
 
 function Hero() {
     const handleScrollToAbout = () => {
@@ -40,6 +51,23 @@ function Hero() {
                 <p className='hero-subtitle'>
                     "Passionate about MERN, Python & data analysis."
                 </p>
+
+                {/* Social & Coding Profiles Section - RENDERED HERE FOR BOTH DESKTOP/MOBILE */}
+                <div className='social-links-container'>
+                    {SOCIAL_LINKS.map((link) => (
+                        <a 
+                            key={link.name} 
+                            href={link.url} 
+                            target="_blank" 
+                            rel="noopener noreferrer" 
+                            aria-label={`Visit ${link.name} profile`}
+                            className='social-link-icon' 
+                        >
+                            <link.icon size={30} />
+                        </a>
+                    ))}
+                </div>
+                {/* END SOCIAL SECTION */}
 
                 <div className='hero-actions'>
                     <button className='btn btn-primary' onClick={handleScrollToAbout}>
